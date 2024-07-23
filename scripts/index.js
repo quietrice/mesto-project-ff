40ff9b5b07bb94b9613acdbb6a52647dbd8b5c4e
Andrey Sh <andrey.krut08@gmail.com>
2024-07-23 13:05:59 +0300

    1.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,38 @@
 // @todo: Темплейт карточки
 
-// @todo: DOM узлы
+const cardTemplate = document.querySelector('#card-template').content;
+const contentMain = document.querySelector('.content')
+const cardContainer = contentMain.querySelector('.places__list')
+const cardElement = cardTemplate.querySelector('.card');
+
+let placesName = ['Анапа', 'Сызрань', 'Волга', 'Пехорка', 'Екатеринбург'];
 
 // @todo: Функция создания карточки
 
+placesName.forEach(function (item) {
+    let index = placesName.indexOf(item);
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__image').src = `./images/place${index+1}.jpg`;
+    cardElement.querySelector('.card__description').textContent = item;
+    console.log(item);
+
+    const resetButton = cardElement.querySelector('.card__delete-button');
+    resetButton.addEventListener('click', removeCard);
+    cardContainer.append(cardElement);
+}
+)
+
+// @todo: DOM узлы
 // @todo: Функция удаления карточки
 
+function removeCard() {
+    console.log('функция удаления');
+    let card = this.closest('.card');
+    card.remove();
+
+    
+}
+
 // @todo: Вывести карточки на страницу
+
+
