6be353ce35677f53c4df89f5086cfbb2765b087c
Andrey Sh <andrey.krut08@gmail.com>
2024-07-23 22:21:48 +0300

    2.0

--- scripts/index.js
+++ scripts/index.js
@@ -3,31 +3,73 @@
 const cardTemplate = document.querySelector('#card-template').content;
 const contentMain = document.querySelector('.content')
 const cardContainer = contentMain.querySelector('.places__list')
-const cardElement = cardTemplate.querySelector('.card');
+// const cardElement = cardTemplate.querySelector('.card');
 
-let placesName = ['Анапа', 'Сызрань', 'Волга', 'Пехорка', 'Екатеринбург'];
+const placesName = ['Анапа', 'Сызрань', 'Волга', 'Пехорка', 'Екатеринбург', 'Астана'];
+
+const cardInfo = [
+    {
+    placeName: '', link: '', alt: '' 
+    },
+    {
+    placeName: '', link: '', alt: '' 
+    }, 
+    {
+    placeName: '', link: '', alt: '' 
+    },
+    {
+    placeName: '', link: '', alt: '' 
+    },
+    {
+    placeName: '', link: '', alt: '' 
+    },
+    {
+    placeName: '', link: '', alt: '' 
+    },
+
+]
+
+
+
+// массив объектов наполняется данными карточек
+cardInfo.forEach((card, index) =>{
+    card.placeName = placesName[index];
+    card.link = `./images/place${index+1}.jpg`;
+    card.alt = `Красоты замечательного места ${placesName[index]}`;
+})
 
 // @todo: Функция создания карточки
+function createCard(item, removeCard) {
+    // тут создаете карточку и возвращаете ее
 
-placesName.forEach(function (item) {
-    let index = placesName.indexOf(item);
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__image').src = item.link;
+    cardElement.querySelector('.card__image').alt = item.alt;
+    cardElement.querySelector('.card__description').textContent = item.placeName;
+    
     const resetButton = cardElement.querySelector('.card__delete-button');
 
-    cardElement.querySelector('.card__image').src = `./images/place${index+1}.jpg`;
-    cardElement.querySelector('.card__description').textContent = item;
-
     resetButton.addEventListener('click', removeCard);
-    cardContainer.append(cardElement);
+    console.log('функция создания карточек использована');
+    return cardElement
+}
+
+cardInfo.forEach(function (item) {
+    let index = placesName.indexOf(item);
+
+    const newCard = createCard(item);
+    cardContainer.append(newCard);
+    console.log('карточка добавлена');
 }
 )
-
 // @todo: DOM узлы
 // @todo: Функция удаления карточки
 
 function removeCard() {
     let card = this.closest('.card');
     card.remove();
+    console.log('удаление')
 }
 
 // @todo: Вывести карточки на страницу
